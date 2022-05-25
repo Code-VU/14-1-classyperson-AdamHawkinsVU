8b791777d0c092b60a642b7b24b6d543326645e8
Adam Hawkins <Adam.Hawkins@vu.com>
2022-05-25 10:43:13 -0500

    First draft

--- classy_person.py
+++ classy_person.py
@@ -9,7 +9,29 @@ don't forget the use of 'self' and to have the methods:
 
 class Person:
 
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+    
+    def increase_age(self):
+        self.age += 1
+        print (self.age)
 
+    def say_greeting(self):
+        print ("Hello world! My name is", self.name, "!")
 
+    def __iter__(self):
+        self.start = 1
+        return self
+    
+    def count_to_age(self):
+        for x in range(1,self.age):
+            print(x)
+
+adam = Person("Adam", 32)
+print (adam.age)
+adam.say_greeting()
+adam.increase_age()
+adam.count_to_age()
 
 # You won't need to call anything here.
